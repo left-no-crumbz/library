bc47ca789c2d44a3256e748b9689a5bc1a4f321d
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-08-23 09:30:33 +0800

    feat: Add addBook functionality

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const openModal = document.getElementById("open-modal");
 const modal = document.getElementById("modal");
 const addBookBtn = document.getElementById("add-book-btn");
 const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const readStatus = document.getElementById("readStatus");
+const form = document.getElementById("book-form");
 
-console.log(modal);
-
-function Book(title, author, pages) {
+function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.hasRead = !!(hasRead.value === "on");
 }
 
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
 
 function addBookToLibrary(book){
     myLibrary.push(book);
@@ -24,13 +30,28 @@ function removeBookToLibrary(book){
 
 }
 
-openModal.addEventListener("mousedown", (event) => {
+addBookBtn.addEventListener("mousedown", (event) => {
+    const bookTitle = title.value;
+    const bookAuthor = author.value;
+    const bookPages = pages.value;
+    const bookReadStatus = readStatus.value;
+
+    const book = new Book(bookTitle, bookAuthor, bookPages, bookReadStatus)
+    addBookToLibrary(book);
+    console.log(myLibrary);
+})
+
+
+openModal.addEventListener("mousedown", () => {
     modal.showModal();
 })
 
 window.addEventListener("mousedown", (event) => {
+    event.preventDefault();
     console.log(event.target);
     if (event.target === modal) {
         modal.close();
     }
 })
+
+console.log(myLibrary);
