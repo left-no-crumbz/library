532ff50df3ea423a8671a1a88dd0c7a250cb0f6a
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2024-08-24 09:08:12 +0800

    feat: Add delete book functionality

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const form = document.getElementById("book-form");
 const bookContainer = document.getElementById("books-container");
 
 
+
+
+
+Book.prototype.toggleRead = () => {
+    //  add toggle read functionality
+}
+
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
@@ -23,6 +30,10 @@ function Book(title, author, pages, hasRead) {
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     modal.close();
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    readStatus.checked = false;
     console.log(myLibrary);
     displayBooks();
 })
@@ -31,40 +42,57 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-function removeBookToLibrary(book){
-
-}
 
 function* generateBooks(){
     let index = 0;
     while (true) {
-        // console.log(myLibrary[index++]);
         yield myLibrary[index++];
+    }
+}
 
+function removeBookToLibrary(element){
+    const card = element;
+    const index = card.getAttribute("data-index");
+    console.log(index);
+    if(index > -1){
+        myLibrary.splice(index, 1);
+        bookContainer.removeChild(element);
     }
 }
 
 function displayBooks() {
     const bookGenerator = generateBooks();
-    const card = document.createElement("div");
-    const title = document.createElement("h1");
-    const author = document.createElement("p");
-    const pages = document.createElement("p");
 
-    
+    // TODO: Add remove button
+    // TODO: Add toggle read button
+    const card = document.createElement("div");
+    const bookTitle = document.createElement("h1");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const removeBtn = document.createElement("button"); 
+    removeBtn.textContent = "Remove";
+
     for (let index = 0; index < myLibrary.length; index++) {
         const book = bookGenerator.next();
-        title.textContent = book.value.title;
-        author.textContent = book.value.author;
-        pages.textContent = book.value.pages;
-        
+
+        bookTitle.textContent = book.value.title;
+        bookAuthor.textContent = book.value.author;
+        bookPages.textContent = book.value.pages;
+        card.classList.add("card");
+        card.setAttribute("data-index", `${index}`)
+
         bookContainer.appendChild(card);
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);        
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(removeBtn);
     }
 
+    removeBtn.addEventListener("mousedown", (event) => {
+        removeBookToLibrary(card);
+    });
 }
+
 
 addBookBtn.addEventListener("mousedown", (event) => {
     const bookTitle = title.value;
@@ -76,13 +104,16 @@ addBookBtn.addEventListener("mousedown", (event) => {
     addBookToLibrary(book);    
 })
 
+
 openModal.addEventListener("mousedown", () => {
     modal.showModal();
 })
 
+
+
 window.addEventListener("mousedown", (event) => {
-    console.log(event.target);
     if (event.target === modal) {
         modal.close();
     }
+
 })
